711a574bcc7cf755e161e9f12094b433af9ebdde
Aryan7701 <aryanagrawal7701@gmail.com>
2023-02-07 19:28:47 +0530

    Daily Web Dev Practice

---
+++ WDWA53.js
@@ -0,0 +1,39 @@
+console.log("This is Javascript Tutorial 53");
+
+function greet(name, Ohiao="Ohiao Gosaimas"){
+    let name1="Akash";
+    console.log(Ohiao + " " + name);
+    console.log(name + " is a good programmer");
+    
+}
+
+function sum(a,b,c){
+    let d = a+ b+c;
+    return d;
+    console.log(name1);
+}
+let name1 ="Aryan";
+let name2 ="Abhinav";
+let name3 ="Adesh";
+let name4 ="Shubham";
+let name5 ="Dev";
+
+let greet1 = "Good Morning";
+
+greet(name1, greet1);
+greet(name2, greet1);
+greet(name3, greet1);
+greet(name4, greet1);
+greet(name5);
+let returnVal = greet(name3);
+console.log(returnVal);
+
+let returnVal1 = sum(1,2,3);
+console.log(returnVal1);
+
+
+//console.log(name1 + " is a good programmer");
+//console.log(name2 + " is a good programmer");
+//console.log(name3 + " is a good programmer");
+//console.log(name4 + " is a good programmer");
+//console.log(name5 + " is a good programmer");
